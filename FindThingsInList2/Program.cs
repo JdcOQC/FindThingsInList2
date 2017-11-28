09fc5e2f4f8e72340b77da33adb3787abc70f9e9
JdcOQC <henry.lin@jasperdisplay.com>
2017-11-28 16:09:56 +0800

    Finish the problem framework

--- FindThingsInList2/Program.cs
+++ FindThingsInList2/Program.cs
@@ -10,7 +10,35 @@ namespace FindThingsInList2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dd");
+            List<String> a = new List<String>();
+            a.Add("心與浮云閑");
+            a.Add("感物動我心");
+            a.Add("拔劍四顧心茫然");
+            a.Add("傾心美酒盡玉碗");
+            a.Add("貴賤結交心不移");
+
+            for(int i = 0; i < a.Count; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine();
+
+            // 請用程式數數看，有幾句詩第一個字是「心」
+            // 提示: https://msdn.microsoft.com/zh-tw/library/baketfxw(v=vs.110).aspx
+            int count_first = 0;
+
+            Console.WriteLine("有 {0} 句詩第一個字是「心」", count_first);
+
+            // 請用程式數數看，有幾句詩最後一個字是「心」
+            // 提示: https://msdn.microsoft.com/zh-tw/library/2333wewz(v=vs.110).aspx
+            int count_last = 0;
+
+            Console.WriteLine("有 {0} 句詩最後一個字是「心」", count_last);
+
+            // 請用程式數數看，有幾句詩裡面有「心」，但不是第一個字，也不是最後一個字
+            int count_middle = 0;
+
+            Console.WriteLine("有 {0} 句詩裡面有「心」，但不是第一個字，也不是最後一個字", count_middle);
 
             Console.ReadKey();
         }
